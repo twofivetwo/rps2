d20adceea96324369a9d721906074175c2e8814c
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-02-27 12:16:56 +0000

    Write function to play a single round against computer

--- scripts.js
+++ scripts.js
@@ -11,4 +11,33 @@ function computerPlay(){
     return computerSelection;
 }
 
-console.log(computerPlay());
+//
+
+// write a function that plays a single round vs computer.
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection === playerSelection && computerSelection === computerSelection) {
+        case playerSelection === 'rock' && computerSelection === 'scissors':
+        case playerSelection === 'scissors' && computerSelection === 'paper':
+        case playerSelection === 'paper' && computerSelection === 'rock':
+            alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+            break;
+        case playerSelection === 'scissors' && computerSelection === 'rock':
+        case playerSelection === 'paper' && computerSelection === 'scissors':
+        case playerSelection === 'rock' && computerSelection === 'paper':
+            alert(`You lose...${computerSelection} beats ${playerSelection}`);
+            break;
+        case playerSelection === 'rock' && computerSelection === 'rock':
+        case playerSelection === 'paper' && computerSelection === 'paper':
+        case playerSelection === 'scissors' && computerSelection === 'scissors':
+            alert('It\'s a tie!');
+            break;
+        default:
+            alert('This is the default message');
+            break;
+    }
+}
+
+playerSelection = prompt('Choose your weapon...').toLowerCase();
+computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
